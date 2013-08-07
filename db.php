aece3a1147d5ef363360a2104f56aea8aaba976c
Lixing Zhang <s3441303@student.rmit.edu.au>
2013-08-07 13:37:45 +1000

    connect to database using PDO and write a basci query

---
+++ db.php
@@ -0,0 +1,14 @@
+<?php
+
+define('DB_NAME', 'winestore');
+define('DB_USER', 'root');
+define('DB_PW', 'root');
+
+$databaseName = DB_NAME;
+$hostName = 'localhost';
+$dsn = "mysql:dbname=$databaseName;host=$hostName;port=80";
+
+
+?>
+
+
---
+++ query.php
@@ -0,0 +1,46 @@
+<?php
+
+require_once('db.php');
+
+if (isset($_GET['wineName'])) {
+  $wineName = $_GET['wineName'];
+}
+
+
+
+
+try {
+  $pdo = new PDO($dsn, DB_USER, DB_PW);
+
+// all errors will throw exceptions
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  $query = "SELECT * FROM wine where wine_name like '%". $wineName . "%'";
+
+  echo $query;
+
+  //die;
+  //$result = $pdo->query($query);
+
+ // also try PDO::FETCH_NUM, PDO::FETCH_ASSOC and PDO::FETCH_BOTH
+  $result = $pdo->query($query, PDO::FETCH_ASSOC);
+
+  echo '<pre>';
+  print_r($result);
+  echo '</pre>';
+
+  foreach ($result as $row) {
+    echo '<pre>';
+    print_r($row);
+    echo '</pre>';
+  }
+
+
+  // close the connection by destroying the object
+  $pdo = null;
+} catch (PDOException $e) {
+  echo $e->getMessage();
+  exit;
+}
+
+?>
