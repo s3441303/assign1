dc2a4e526adea200b1929d44b40d66900d88a1db
Lixing Zhang <s3441303@student.rmit.edu.au>
2013-08-31 21:51:03 +1000

    Final commit

--- viewedWine.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-$index = intval($_GET['id'])-1;
-
-foreach ($_SESSION['viewedWines'][$index] as $key=>$viewedWines) {
-        echo '<tr><td>'.$viewedWines.'</td></tr>';
-    }
-
-?>
